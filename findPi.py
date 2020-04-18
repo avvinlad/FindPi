73ac8bb2c5ec25f67a0bcbe3a7b57ece097049c8
Avvinlad <avin@hipposhq.com>
2020-04-18 18:14:16 -0400

    findPi file change

--- findPi.py
+++ findPi.py
@@ -1,26 +1,11 @@
-# Estimate pi with given random value 
-import random
+from estimate_pi import estimate_pi
 
 def main():
     total = int(input("Number of Data Points (0 to exit): "))
+
     while (total != 0):
         print("Estimate of Pi: {:f}".format(estimate_pi(total)))
         total = int(input("Number of Data Points (0 to exit): "))
-
-
-def estimate_pi(n):
-    num_circle = 0
-    pi = 0
-
-    for _ in range(n):
-        x = random.uniform(0, 1)
-        y = random.uniform(0, 1)
-        dist = x**2 + y**2
-        
-        if dist <= 1:
-            num_circle += 1
-    
-    return (4 * (num_circle / n))
 
 
 if __name__ == "__main__":
